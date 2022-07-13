917e12d358319fad260c765c8a9db16a59a70ef8
Neh Patel <75005384+thecyberneh@users.noreply.github.com>
2022-07-13 14:47:19 +0530

    Delete iprecon.sh

--- iprecon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#iprecon
-
-
-shodan search "ssl:.target.com  200" --fields ip_str | tee IPs.txt 
-python3 com.py
-rustscan -a IPs.txt -r 1-65535 | grep Open | tee open_ports.txt 
-cat open_ports.txt | sed 's/Open //' | httpx -silent | nuclei -t ~/nuclei-templates/
