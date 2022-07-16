ca89c2bb9ec59c6203fae276e04267411412827a
Neh Patel <75005384+thecyberneh@users.noreply.github.com>
2022-07-16 11:07:26 +0530

    Update iprecon.sh

--- iprecon.sh
+++ iprecon.sh
@@ -19,7 +19,7 @@ printf ""$by"
 '####: ##:::::::: ##:::. ##: ########:. ######::. #######:: ##::. ##:
 ....::..:::::::::..:::::..::........:::......::::.......:::..::::..:: 
                                        
-        V1.1.0
+        V2.1.0
         Coded By Neh Patel with Love <3 ❤ 
                                                                          
                                                                                      
@@ -40,22 +40,16 @@ echo -e "\n"
 echo -e "$rl"[CRITICAL]"$rle" "$by"Configure shodan with your api key before running "$bye"
 echo -e "\n"
 echo -e "$by"[INFO] Starting scan for IPs "$bye"
-shodan search "hostname:.$1  200" --fields ip_str | tee IPs.txt 
+shodan search "hostname:.$1  200" --limit 1000 --fields ip_str | tee IPs.txt 
 echo -e "\n"
 echo -e "$by"[INFO] All IPs are saved in file: IPs.txt "$bye"
 echo -e "\n"
-
-for i in `cat IPs.txt`;
-do
-        echo "$i""," >> forRust1.txt
-done
-cat forRust1.txt | sed '$s/,$//' | tee forRust.txt
+cat IPs.txt | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | tee forRust.txt
 echo -e "\n"
-echo -e "$by"[INFO] Ready for rustscan with file: forRust.txt "$bye"
+echo -e "$by"[INFO] Scanning for open ports "$bye"
+rustscan -a forRust.txt -r 1-65535 --ulimit 5000 --scripts none | tee rustscanRes.txt
 echo -e "\n"
-rustscan -a forRust.txt -r 1-65535 --ulimit 5000 --scripts none | grep Open | tee open_ports.txt 
-echo -e "\n"
-echo -e "$by"[INFO] Ready for nuclei with file: open_ports.txt "$bye"
+cat rustscanRes.txt | grep Open | tee open_ports.txt 
 echo -e "\n"
 cat open_ports.txt | sed 's/Open //' | httpx -silent | tee IPUrls.txt
 echo -e "\n"
